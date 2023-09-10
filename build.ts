f6abafaf1da702f8f3e6e6f83221cf9c82a7c43c
Justin Grote <JustinGrote@users.noreply.github.com>
2023-09-09 22:31:24 -0700

    Checkpoint before I try a new substitution

--- build.ts
+++ build.ts
@@ -5,19 +5,58 @@ import { load } from "js-yaml"
 
 const basefile = 'log.tmLanguage'
 type Variables = Record<string, string>
+type Resolvers = Map<string, string>
 type Grammar = {
 	variables?: Variables
 }
+const tokenBegin = '\\$\\{'
+const tokenEnd = '\\}'
 
-// Replace {{variable}} with the variable content
-function replaceVariables(content: string, variables: Variables): string {
-	return content.toString().replace(/<<<\b([\w-]+?)>>>/g, (_, name) => {
-		const value = variables[name]
-		if (value === undefined) {
-			throw new Error(`Unknown variable ${name}`)
+/** Recursively resolves all variable values that may reference each other */
+function resolveVariables(variables: Variables) {
+	// If a variable contains a substitution, replace it with that substitution
+	const resolved: Resolvers = new Map()
+	const resolving: Resolvers = new Map()
+
+	Object.entries(variables).forEach(([name, template]) => {
+		if (!template.match(tokenBegin)) {
+			console.log(`RESOLVER: ${name} will resolve to ${template}`)
+			resolved.set(name, template)
+		} else {
+			resolving.set(name, template)
 		}
-		console.log(`Replacing variable ${name} with ${value}`)
-		return value
+	})
+
+	while (resolving.size > 0) {
+		let newResolverDetected = false
+		resolving.forEach((template, name) => {
+			const resolvedTemplate = replaceVariables(template, resolved)
+			if (resolvedTemplate.match(tokenBegin)) {
+				// We still have unresolved variables for this entry, we will try again on the next loop
+				return
+			}
+			newResolverDetected = true
+			console.log(`RESOLVER: ${name} will resolve to ${resolvedTemplate}`)
+			resolved.set(name, resolvedTemplate)
+			resolving.delete(name)
+		})
+
+		if (!newResolverDetected) {
+			throw new Error(`Either these strings have variables that are not defined, or a circular reference exists: ${JSON.stringify(resolving)}`)
+		}
+	}
+
+	return resolved
+}
+
+function replaceVariables(content: string, resolvers: Resolvers): string {
+	const substitutionRegex = `${tokenBegin}\\b([\\w-]+?)${tokenEnd}`
+	return content.toString().replaceAll(new RegExp(substitutionRegex, "g"), (match, name) => {
+		const resolvedValue = resolvers.get(name)
+		// If the variable is not defined, leave it as-is
+		if (!resolvedValue) { return match }
+		console.log(`Replacing variable ${name} with ${resolvedValue}`)
+		return resolvedValue
 	})
 }
 
@@ -26,7 +65,7 @@ let grammar = load(grammarYaml) as Grammar
 
 const variables = grammar.variables
 if (variables) {
-	grammar = load(replaceVariables(grammarYaml, variables)) as Grammar
+	grammar = load(replaceVariables(grammarYaml, resolveVariables(variables))) as Grammar
 }
 
 delete grammar.variables
